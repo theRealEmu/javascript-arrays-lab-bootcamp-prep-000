8bbd44ee5a4352bb9164266fe882587d53db5cad
Emanuel Harms <emanuel.harms@gmx.de>
2018-10-14 07:16:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 
-//1 
+//1 destructivelyAppendKitten
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  reutrn kittens;
+}
